8486966f07ebdfe6e80843fef4340822a3768e42
EvgenyRazumov <razumovhome@gmail.com>
2015-11-28 23:39:34 +0300

    added Tests project.

---
+++ src/QaStories.Tests/SimpleOne.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QaStories.Tests
+{
+    public class SimpleOne
+    {
+        [Test]
+        public void Test01()
+        {
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void Test02()
+        {
+            Assert.AreEqual(1, 0);
+        }
+    }
+}

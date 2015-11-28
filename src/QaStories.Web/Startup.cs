ba2af34895042f651c4d8f10c0506c26a58d85e3
EvgenyRazumov <razumovhome@gmail.com>
2015-11-28 23:28:41 +0300

    initial commit.

---
+++ src/QaStories.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QaStories.Web.Startup))]
+namespace QaStories.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
